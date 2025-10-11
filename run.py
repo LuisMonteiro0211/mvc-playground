d6b66a3a4cb4eefb71449c04d29f988b1fa790d4
Luis Felipe de Oliveira Monteiro <luisfelipeomonteiro@gmail.com>
2025-10-11 13:53:43 -0300

    Começando do zero a aplicação

--- run.py
+++ run.py
@@ -1,6 +1,1 @@
-from src.main.process_handle import start
-import os
 
-if __name__ == '__main__':
-    os.system('cls')
-    start() 
--- src/main/constructor/introduction_process.py
+++
@@ -1,6 +0,0 @@
-from src.views.first_view import introduction_page
-
-# Recebe a entrada do usuário e retorna o comando
-def introduction_process():
-    command = introduction_page()
-    return command
